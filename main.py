3180a34c4ce841ce4b09c2763cb5e66b83541b80
Thomas Flerko <thomas.flerko@yandex.com>
2025-07-09 17:40:25 +0200

    Update ..

--- main.py
+++ main.py
@@ -248,14 +248,14 @@ def edit_post(post_id):
         subtitle=post.subtitle,
         img_url=post.img_url,
         author=post.author,
-        body=post.body
+        body=Markup(post.body)
     )
     if edit_form.validate_on_submit():
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
         post.author = current_user
-        post.body = edit_form.body.data
+        post.body = Markup(edit_form.body.data)
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
     return render_template("make-post.html", form=edit_form, is_edit=True, current_user=current_user)
